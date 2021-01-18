f9248a61da74bcadefff46253cc27bde53618766
fxPracht <felixpracht@gmail.com>
2021-01-18 21:36:00 +0100

    Fixed a bug where the cursors weren't drawn correctly

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from threading import Thread
 from customListener import CustomListener
 import pygame
 import math
-
+import copy
 
 display_width = 800
 display_height = 800
@@ -34,7 +34,7 @@ def test_dispatcher_listener():
 
 def drawCursors(positions, screen):
     touch_size = 5
-    for value in positions.cursors.values():
+    for value in positions.values():
         if len(value) != 2:
             return
         (x, y) = value
@@ -48,7 +48,7 @@ def init():
     pygame.font.init()
     pygame.fastevent.init()
     clock = pygame.time.Clock()
-    screen = pygame.display.set_mode((display_width, display_height))
+    screen = pygame.display.set_mode((display_width, display_height), 0, 32)
 
     refresh_event = custom_listener_obj.pygame_refresh_event
 
@@ -56,31 +56,45 @@ def init():
 
     done = False
     while not done:
+
         screen.fill([150, 150, 150])
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 done = True
             elif event.type == refresh_event:
                 old_cursors = on_refresh(custom_listener_obj, screen, old_cursors)
+        drawCursors(old_cursors, screen)
 
         pygame.display.update()
         dt = clock.tick(30)
+
     pygame.quit()
 
 
 def on_refresh(custom_listener_obj, screen, old_cursors):
-    drawCursors(custom_listener_obj, screen)
-    current_cursors = custom_listener_obj.cursors.copy()
+    current_cursors = copy.deepcopy(custom_listener_obj.cursors)
 
     if len(current_cursors) == 2 and len(old_cursors) == 2:
         if set(current_cursors.keys()) == set(old_cursors.keys()):
             (key1, key2) = current_cursors.keys()
-            key1_point_new = current_cursors.get(key1)
-            key1_point_old = old_cursors.get(key1)
-            key2_point_new = current_cursors.get(key2)
-            key2_point_old = old_cursors.get(key2)
+
+            (x, y) = current_cursors.get(key1)
+            key1_point_new = (x * display_width, y * display_height)
+
+            (x, y) = old_cursors.get(key1)
+            key1_point_old = (x * display_width, y * display_height)
+
+            (x, y) = current_cursors.get(key2)
+            key2_point_new = (x * display_width, y * display_height)
+
+            (x, y) = old_cursors.get(key2)
+            key2_point_old = (x * display_width, y * display_height)
+
             distance_new = math.dist(key1_point_new, key2_point_new)
             distance_old = math.dist(key1_point_old, key2_point_old)
+            print(key1_point_new, key2_point_new)
+            print(key1_point_old, key2_point_old)
             print(distance_old, distance_new)
 
     return current_cursors
