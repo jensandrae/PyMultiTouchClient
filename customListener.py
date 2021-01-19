b17eab25dd8f265a8e696987a37cd1f64f1abdfd
Jens Andrae <andrae.dev@posteo.de>
2021-01-19 22:47:06 +0100

    Added dollar algorithm and basic gesture detection, not finished now, square works.

--- customListener.py
+++ customListener.py
@@ -88,37 +88,3 @@ class CustomListener(TuioListener):
             print("----------------------------------------------")
         ev = self.pygame.event.Event(self.pygame_refresh_event)
         self.pygame.fastevent.post(ev)
-
-# ToDo(1) ● Implementierung der TuioClient API # (DONE)
-# ToDo(2) ● Auswerten der TUIO Events # (DONE)
-# ToDo(3) ● Erstellen einer eigenen Gesten-Alogrhytmik zur Interaktion mit 2-Dimensionalen Objekten (translate, rotate, scale) (Praktikum 4+5)
-# ToDo(4) ● Erweiterte Gestenerkennungen (Praktikum 4+5) -> Diskrete Gesten sind gemeint !
-
-#### Zwischen ToDo's für 2D Interaktion
-# ToDo(3.1) ● getCourserId()
-# ToDo(3.2) ● getSessionId()
-# ToDo(3.3) ● getX()
-# ToDo(3.4) ● getY()
-# ToDo(3.5) ● translate() -> Kont. Gesten
-# ToDo(3.6) ● rotate() -> Kont. Gesten
-# ToDo(3.8) ● Erweiterte Gestenerkennung (MUSS ?) -> Nicht Kontin. Gesten -> Sehr relevant für Benotung -> Keine False Positives idealerweise
-# ToDo(3.9) ● Normalisierung der Geste + Reduktion auf 32 Punkte einer unrealistischen Geste
-
-
-# 1. Kein Objekt
-# 2. Wenn per wer. GEsten etwas erkannt wird dann zeichen allgemein, nicht so wie user (Das gilt immer auch wenn schon was da ist)
-# 3. Kont. Gesten anwenden
-
-# Objekte -> Dreieck, Rechteck, Kreis
-
-## Objekte aus Geste Erkennen -> Erstellen -> Löschen ->
-## Es reicht simple impl. sauberes erkennen von Gesten !!! -> Klare erkennung von Geste ist relevant, was damit gesteuert wird nicht, es muss nur eindeutig sein !!!
-
-## Letzer Termin als Arbeitstermin -> Wenn nicht fertig, dann seperater Termin zur Abgabe (ca. 14 Tage danach) !! -> Abgabe ist aber trotzdem möglich !!
-
-#### FRAGE AN PROF
-# Müssen verschiedene Objekte vorhanden sein die seperat gesteuert werden können sollten ?
-#
-
-
-##### UNITY -> Erscheinen lassen von Objekten und diese seperat Steuern ?!
--- dollar.py
+++ dollar.py
@@ -1,19 +1,5 @@
-#
-# Usage example:
-#
-# from dollar import Recognizer
-#
-# r = Recognizer()
-# r.addTemplate('square', [(1, 10), (3, 8) ... ])
-# r.addTemplate('circle', [(4, 7), (5, 13) ... ])
-#
-# (name, score) = r.recognize([(5, 6), (7, 12), ... ])
-#
-
 import math
 import operator
-
-# Contants. Tweak at your peril. :)
 
 numPoints = 64
 squareSize = 250.0
@@ -24,7 +10,9 @@ phi = 0.5 * (-1.0 + math.sqrt(5.0))  # Golden Ratio
 
 
 class Recognizer:
-    """The $1 gesture recognizer"""
+    """The $1 gesture recognizer.
+   See http://sleepygeek.org/projects.dollar for more, ==> no more available
+   or http://depts.washington.edu/aimgroup/proj/dollar/ for the original implementation and paper."""
 
     def __init__(self):
         self.templates = []
@@ -40,26 +28,26 @@ class Recognizer:
         is the matching template, and score is a float [0..1] representing the match certainty. """
 
         points = [Point(point[0], point[1]) for point in points]
-        points = _resample(points, numPoints)
-        points = _rotate_to_zero(points)
-        points = _scale_to_square(points, squareSize)
-        points = _translate_to_origin(points)
+        points = _resample(points, numPoints);
+        points = _rotateToZero(points);
+        points = _scaleToSquare(points, squareSize);
+        points = _translateToOrigin(points);
 
-        best_distance = float("infinity")
-        best_template = None
+        bestDistance = float("infinity")
+        bestTemplate = None
         for i, template in enumerate(self.templates):
-            distance = _distance_at_best_angle(points, template, -angleRange, +angleRange, anglePrecision)
+            distance = _distanceAtBestAngle(points, template, -angleRange, +angleRange, anglePrecision)
             self.distances[i] = distance
-            if distance < best_distance:
-                best_distance = distance
-                best_template = template
+            if distance < bestDistance:
+                bestDistance = distance
+                bestTemplate = template
 
-        score = 1.0 - (best_distance / halfDiagonal)
+        score = 1.0 - (bestDistance / halfDiagonal)
         self.scores = [1.0 - (d / halfDiagonal) for d in self.distances]
 
-        return best_template.index, best_template.name, score
+        return (bestTemplate.index, bestTemplate.name, score)
 
-    def add_template(self, name, points):
+    def addTemplate(self, name, points):
         """Add a new template, and assign it a name. Multiple templates can be given the same name, for more accurate
         matching. Returns an integer representing the number of templates matching this name. """
         self.templates.append(Template(name, points, len(self.templates)))
@@ -78,31 +66,31 @@ class Recognizer:
         # Return the number of templates with this name.
         return len([t for t in self.templates if t.name == name])
 
-    def add_templates(self, data_dict):
+    def addTemplates(self, data_dict):
         for one_type in data_dict.keys():
             if type(data_dict[one_type][0]) == list:
                 for one_data in data_dict[one_type]:
-                    self.add_template(one_type, one_data)
+                    self.addTemplate(one_type, one_data)
             else:
-                self.add_template(one_type, data_dict[one_type])
+                self.addTemplate(one_type, data_dict[one_type])
 
-    def delete_templates(self, name):
+    def deleteTemplates(self, name):
         """Remove all templates matching a given name. Returns an integer representing the new number of templates."""
 
         self.templates = [t for t in self.templates if t.name != name]
-        return len(self.templates)
+        return len(self.templates);
 
-    def get_template_name(self, remove_duplicate=False):
-        gesture_name_dict = dict()
+    def getTemplateNames(self, remove_duplicate=False):
+        gestureName_dict = dict()
 
         for i, one_template in enumerate(self.templates):
-            if remove_duplicate and one_template.name in gesture_name_dict.values():
+            if remove_duplicate and one_template.name in gestureName_dict.values():
                 continue
-            gesture_name_dict[i] = one_template.name
+            gestureName_dict[i] = one_template.name
 
-        return gesture_name_dict
+        return gestureName_dict
 
-    def get_one_score_by_gesture(self):
+    def getOneScrorebyGesture(self):
         scores = []
         start_index = 0
         for one_key in self.num_templates.keys():
@@ -135,23 +123,22 @@ class Template:
     """A gesture template. Used internally by Recognizer."""
 
     def __init__(self, name, points, index=-999):
-        """'name' is a label identifying this gesture, and 'points' is a list of tuple co-ordinates representing the
-        gesture positions. Example: [(1, 10), (3, 8) ...] """
+        """'name' is a label identifying this gesture, and 'points' is a list of tuple co-ordinates representing the gesture positions. Example: [(1, 10), (3, 8) ...]"""
         self.name = name
         self.points = [Point(point[0], point[1]) for point in points]
-        self.points = _resample(self.points, numPoints)
-        self.points = _rotate_to_zero(self.points)
-        self.points = _scale_to_square(self.points, squareSize)
-        self.points = _translate_to_origin(self.points)
+        self.points = _resample(self.points, numPoints);
+        self.points = _rotateToZero(self.points);
+        self.points = _scaleToSquare(self.points, squareSize);
+        self.points = _translateToOrigin(self.points);
 
-        self.index = index
+        self.index = index;
 
 
 def _resample(points, n):
     """Resample a set of points to a roughly equivalent, evenly-spaced set of points."""
-    I = _path_length(points) / (n - 1)  # interval length
+    I = _pathLength(points) / (n - 1)  # interval length
     D = 0.0
-    new_points = [points[0]]
+    newpoints = [points[0]]
     i = 1
     while i < len(points) - 1:
         d = _distance(points[i - 1], points[i])
@@ -159,7 +146,7 @@ def _resample(points, n):
             qx = points[i - 1].x + ((I - D) / d) * (points[i].x - points[i - 1].x)
             qy = points[i - 1].y + ((I - D) / d) * (points[i].y - points[i - 1].y)
             q = Point(qx, qy)
-            new_points.append(q)
+            newpoints.append(q)
             # Insert 'q' at position i in points s.t. 'q' will be the next i
             points.insert(i, q)
             D = 0.0
@@ -168,61 +155,60 @@ def _resample(points, n):
         i += 1
 
     # Sometimes we fall a rounding-error short of adding the last point, so add it if so.
-    if len(new_points) == n - 1:
-        new_points.append(points[-1])
-    return new_points
+    if len(newpoints) == n - 1:
+        newpoints.append(points[-1])
+    return newpoints;
 
 
-def _rotate_to_zero(points):
+def _rotateToZero(points):
     """Rotate a set of points such that the angle between the first point and the centre point is 0."""
     c = _centroid(points)
     theta = math.atan2(c.y - points[0].y, c.x - points[0].x)
-    return _rotate_by(points, -theta)
+    return _rotateBy(points, -theta)
 
 
-def _rotate_by(points, theta):
+def _rotateBy(points, theta):
     """Rotate a set of points by a given angle."""
-    c = _centroid(points)
-    cos = math.cos(theta)
-    sin = math.sin(theta)
+    c = _centroid(points);
+    cos = math.cos(theta);
+    sin = math.sin(theta);
 
-    new_points = []
+    newpoints = [];
     for point in points:
         qx = (point.x - c.x) * cos - (point.y - c.y) * sin + c.x
         qy = (point.x - c.x) * sin + (point.y - c.y) * cos + c.y
-        new_points.append(Point(qx, qy))
-    return new_points
+        newpoints.append(Point(qx, qy))
+    return newpoints
 
 
-def _scale_to_square(points, size):
+def _scaleToSquare(points, size):
     """Scale a scale of points to fit a given bounding box."""
-    B = _bounding_box(points)
-    new_points = []
+    B = _boundingBox(points)
+    newpoints = []
     for point in points:
         qx = point.x * (size / B.width)
         qy = point.y * (size / B.height)
-        new_points.append(Point(qx, qy))
-    return new_points
+        newpoints.append(Point(qx, qy))
+    return newpoints
 
 
-def _translate_to_origin(points):
+def _translateToOrigin(points):
     """Translate a set of points, placing the centre point at the origin."""
     c = _centroid(points)
-    new_points = []
+    newpoints = []
     for point in points:
         qx = point.x - c.x
         qy = point.y - c.y
-        new_points.append(Point(qx, qy))
-    return new_points
+        newpoints.append(Point(qx, qy))
+    return newpoints;
 
 
-def _distance_at_best_angle(points, T, a, b, threshold):
-    """Search for the best match between a set of points and a template, using a set of tolerances. Returns a float
-    representing this minimum distance. """
+def _distanceAtBestAngle(points, T, a, b, threshold):
+    """Search for the best match between a set of points and a template, using a set of tolerances. Returns a float representing this minimum distance."""
     x1 = phi * a + (1.0 - phi) * b
-    f1 = _distance_at_angle(points, T, x1)
+    f1 = _distanceAtAngle(points, T, x1)
     x2 = (1.0 - phi) * a + phi * b
-    f2 = _distance_at_angle(points, T, x2)
+    f2 = _distanceAtAngle(points, T, x2)
 
     while abs(b - a) > threshold:
         if f1 < f2:
@@ -230,20 +216,20 @@ def _distance_at_best_angle(points, T, a, b, threshold):
             x2 = x1
             f2 = f1
             x1 = phi * a + (1.0 - phi) * b
-            f1 = _distance_at_angle(points, T, x1)
+            f1 = _distanceAtAngle(points, T, x1)
         else:
             a = x1
             x1 = x2
             f1 = f2
             x2 = (1.0 - phi) * a + phi * b
-            f2 = _distance_at_angle(points, T, x2)
+            f2 = _distanceAtAngle(points, T, x2)
     return min(f1, f2)
 
 
-def _distance_at_angle(points, T, theta):
+def _distanceAtAngle(points, T, theta):
     """Returns the distance by which a set of points differs from a template when rotated by theta."""
-    new_points = _rotate_by(points, theta)
-    return _path_distance(new_points, T.points)
+    newpoints = _rotateBy(points, theta)
+    return _pathDistance(newpoints, T.points)
 
 
 def _centroid(points):
@@ -258,7 +244,7 @@ def _centroid(points):
     return Point(x, y)
 
 
-def _bounding_box(points):
+def _boundingBox(points):
     """Returns a Rectangle representing the bounding box that contains the given set of points."""
     minX = float("+Infinity")
     maxX = float("-Infinity")
@@ -278,17 +264,17 @@ def _bounding_box(points):
     return Rectangle(minX, minY, maxX - minX, maxY - minY)
 
 
-def _path_distance(pts1, pts2):
+def _pathDistance(pts1, pts2):
     """'Distance' between two paths."""
-    d = 0.0
+    d = 0.0;
     for index in range(min(len(pts1), len(pts2))):  # assumes pts1.length == pts2.length
         d += _distance(pts1[index], pts2[index])
     return d / len(pts1)
 
 
-def _path_length(points):
+def _pathLength(points):
     """Sum of distance between each point, or, length of the path represented by a set of points."""
-    d = 0.0
+    d = 0.0;
     for index in range(1, len(points)):
         d += _distance(points[index - 1], points[index])
     return d
